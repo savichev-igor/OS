50da8e0bb8c5b342bfc8b85ea629e2ad7c1fa364
Savi <Ultimate95@mail.ru>
2017-05-01 13:10:57 +0500

    fixes

--- 03/main.c
+++ 03/main.c
@@ -1,14 +1,18 @@
 #include <glib.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>  // strerror
 #include <errno.h>
 #include <regex.h>
 #include <stdlib.h>  // malloc
 
-void print_error(char *info) {
+void print_error(char *info, bool exit_flag) {
     printf("%s: %s\n", strerror(errno), info);
-    exit(1);
+
+    if (exit_flag) {
+        exit(1);
+    }
 }
 
 int int_sort_function(gconstpointer a, gconstpointer b) {
@@ -22,47 +26,50 @@ void parse_file(char *file_name, GArray *array) {
     FILE *fp = fopen(file_name, "r");
 
     if (errno != 0) {
-        print_error(file_name);
-    }
+        print_error(file_name, false);
+    } else {
+        char *line = NULL;
+        size_t len = 0;
 
-    char *line = NULL;
-    size_t len = 0;
+        regex_t re;
+        regmatch_t match;
 
-    regex_t re;
-    regmatch_t match;
+        regcomp(&re, "-?[0-9]+", REG_EXTENDED);
 
-    regcomp(&re, "-?[0-9]+", REG_EXTENDED);  // compile regexp
+        while ((getline(&line, &len, fp)) != -1) {
+            while (regexec(&re, line, 1, &match, 0) == 0) {
+                size_t len_match = (size_t) (match.rm_eo - match.rm_so);
 
-    if (errno != 0) {
-        print_error("Regexp compile fail");
-    }
+                if ((int) len_match < 10) {
+                    char *buffer = (char *) malloc(len_match);
 
-    while ((getline(&line, &len, fp)) != -1) {
-        while (regexec(&re, line, 1, &match, 0) == 0) {
-            size_t len_match = (size_t) (match.rm_eo - match.rm_so);
-            char *buffer = (char*) malloc(len_match);
+                    if (errno != 0) {
+                        print_error("malloc fail", true);
+                    }
 
-            if (errno != 0) {
-                print_error("Memory fail");
+                    memcpy(buffer, line + match.rm_so, len_match);
+
+                    int num = atoi(buffer);
+                    printf("%d\n", num);
+
+                    g_array_append_val(array, num);
+
+                    free(buffer);
+                }
+
+                line += match.rm_eo;
             }
 
-            memcpy(buffer, line + match.rm_so, len_match);
-
-            int num = atoi(buffer);
-            free(buffer);
-
-            g_array_append_val(array, num);
-
-            line += match.rm_eo;
+            line = NULL;
+            len = 0;
         }
-    }
 
-    fclose (fp);
+        fclose(fp);
+    }
 }
 
 int main(int argc, char *argv[]) {
     GArray *array = g_array_new(FALSE, FALSE, sizeof(int));
-
 
     for (int i = 1; i < argc - 1; i++) {
         parse_file(argv[i], array);
@@ -72,10 +79,10 @@ int main(int argc, char *argv[]) {
 
     char *result_file_name = argv[argc - 1];
 
-    FILE * result_fp = fopen(result_file_name, "wa");
+    FILE *result_fp = fopen(result_file_name, "wa");
 
     if (errno != 0) {
-        print_error(result_file_name);
+        print_error(result_file_name, true);
     }
 
     for (int i = 0; i < array->len; i++) {
